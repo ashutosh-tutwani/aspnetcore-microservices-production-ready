2a59023b6e3ed9802a82969b6a6e27e2defe15fa
Ashutosh Tutwani <133757613+ashutosh-tutwani@users.noreply.github.com>
2023-08-11 18:26:56 +0530

    added swagger services

---
+++ Common/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+
+namespace Services.Common.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddSwagger(this IServiceCollection services,
+                                                                 string xmlPath)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.CustomSchemaIds(x => x.FullName);
+                //Swagger OAuth Authentication
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Please insert JWT with Bearer into field",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            },
+                            Scheme = "oauth2",
+                            Name = "Bearer",
+                            In = ParameterLocation.Header
+                        },
+                        new List<string>()
+                    }
+                });
+                //To be able to use SwashBuckle Annotations
+                c.EnableAnnotations();
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "User Mangement API", Version = "1" });
+                c.IncludeXmlComments(xmlPath, includeControllerXmlComments: true);
+            });
+            return services;
+        }
+    }
+}
